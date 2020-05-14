e5d2f7a5a8c21a3f53a237c2e031d208f912b258
Mario McGee <mario.mcgee@students.makeschool.com>
2020-05-14 12:51:56 -0700

    code syntax fixed

--- coding_syntax.js
+++ coding_syntax.js
@@ -4,14 +4,19 @@
 //the code does not currently run 
 
 
-
-function FizzBuzz(targetnum){
-	for(var i=1; i<targetnum; i++;){
-	   let result = “ ”;
-	   if  ( i%3 === 0 ) result += “Fizz”;
-	   if  ( i%5 === 0 ) result += “Buzz”;
-	   if (  I%3 > 0 &  i%5 > 0) result = i
-Console.log(result += “\n”);
-}
-}
-FizzBuzz(50)
+//syntax corrected looking at js syntax documentation 
+function FizzBuzz(target) {
+    for (let i = 1; i <= target; i += 1) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+        console.log('Fizz');
+      } else if (i % 5 === 0) {
+        console.log('Buzz');
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  
+  FizzBuzz(15);
